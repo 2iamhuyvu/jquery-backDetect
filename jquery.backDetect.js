473c694e17d13c1fdf1a4132e5820cb047034b7f
Ian <ian.rogren@gmail.com>
2018-01-15 19:52:09 -0800

    removed lingering console.log message

--- jquery.backDetect.js
+++ jquery.backDetect.js
@@ -21,7 +21,6 @@
     frameDataSrc: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC',
   };
 
-  console.log('ttttt');
   /**
    * Back Detect
    *
